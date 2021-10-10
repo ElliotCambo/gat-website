a706b0ac93022b7bc9589a134ff6caae874be0ca
Elliot Campbelton <e.b.campbelton@gmail.com>
2021-10-10 13:52:21 +0100

    yhui

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+// const bodyParser = require('body-parser');
+var mysql = require("mysql");
+var postmark = require("postmark");
+var app = express();
+var router = express.Router();
+var pathToGATWebsiteStatic = __dirname + '/public/';
+const uuidv4 = require('uuid/v4');
+
+app.use("/gat", express.static(pathToGATWebsiteStatic));
+app.use(express.json());
+
+router.get("/*",function(req,res){
+        console.log(pathToGATWebsiteStatic + "index.html")
+        res.sendFile(pathToGATWebsiteStatic + "index.html");
+
+});
+
+app.use("/",router);
+
+app.listen(80,function(){
+  console.log("Live at Port 80");
+});
